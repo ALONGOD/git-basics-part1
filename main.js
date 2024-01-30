5692209a65549bb3c43bd38bfcead73431948e17
ALONGOD <alongoddard1@gmail.com>
2024-01-30 10:39:44 +0200

    limit ball size to 400px

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 var initialSize = 100
 function onBallClick(elBall) {
     initialSize += 50
+    if (initialSize === 400) initialSize = 100
     elBall.style.width = initialSize + "px"
     elBall.style.height = initialSize + "px"
+    elBall.innerText = initialSize
+
 }
